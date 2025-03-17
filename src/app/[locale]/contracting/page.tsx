fad68e9ab4617a93df5ffde0b44b03c69f5433fa
Moneer A. Mizyed <xv.neer@gmail.com>
2025-03-17 12:54:08 +0200

    handle meta data

--- src/app/[locale]/contracting/page.tsx
+++ src/app/[locale]/contracting/page.tsx
@@ -1,10 +1,22 @@
 import React from "react"
 import { Hero } from "./_components/hero"
 import { About } from "./_components/about"
+import { getTranslations } from "next-intl/server"
+import { Metadata } from "next"
+import { steelLogo } from "@/assets"
 
-type Props = {}
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations("contracting")
+  return {
+    title: t("title"),
+    description: t("about.description"),
+    icons: {
+      icon: steelLogo.src,
+    },
+  }
+}
 
-const Page = (props: Props) => {
+const Page = () => {
   return (
     <>
       <Hero />
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,19 @@
+import { getTranslations } from "next-intl/server"
 import { AboutUs } from "./_components/about-us"
 import { ContactUs } from "./_components/contact-us"
 import { Facts } from "./_components/facts"
 import Gate from "./_components/gate"
 import { Hero } from "./_components/hero"
 import { Sections } from "./_components/sections"
+import { Metadata } from "next"
 
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations("home")
+  return {
+    title: t("title"),
+    description: t("description"),
+  }
+}
 export default function Home() {
   return (
     <>
